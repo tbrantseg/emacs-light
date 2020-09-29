f1d5c820fb16f0bda7d67a57dc014673da2b4696
BCQ9 <tom.brantseg@semanticbits.com>
2020-09-29 09:59:57 -0500

    org tweaks

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-  ;; Bootstrap use-package
+;; Bootstrap use-package
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -50,28 +50,27 @@
 		    :height 110
 		    :weight 'normal)
 
-(use-package darkburn-theme
-  :ensure t
-  :config
-  (load-theme 'darkburn t))
-
-(use-package smart-mode-line-powerline-theme
+(use-package smart-mode-line-atom-one-dark-theme
   :ensure t)
 
 (use-package smart-mode-line
   :ensure t
   :init
-  (setq sml/no-confirm-load-theme t)
-  (setq sml/theme 'powerline)
+  (setq sml/theme 'atom-one-dark)
   :config
   (sml/setup))
+
+(use-package atom-one-dark-theme
+  :ensure t
+  :config
+  (load-theme 'atom-one-dark t))
 
 (unless window-system
   (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode)
- (scroll-bar-mode -1))
+    (scroll-bar-mode -1))
 (visual-line-mode 1)
 
 (use-package nlinum
@@ -82,6 +81,11 @@
   :bind
   (("<M-f10>" . nlinum-mode)
    ("<M-RET>" . comment-indent-new-line)))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 ;; Helm
 (use-package helm
@@ -121,6 +125,13 @@
   :defer t
   :bind
   (("C-x g" . magit-status)))
+
+(use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server -t)
+  :hook (python . (lambda ()
+		    (require 'lsp-python-ms)
+		    (lsp))))
 
 ;; Company
 (use-package company
@@ -241,22 +252,103 @@
    ("C-c e" . tb-info-hydra/body)
    ("C-c o" . hydra-outline/body)))
 
+;; Update todo state when sub-tasks done
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all sub-entries are done and to TODO otherwise"
+  (let (org-log-done org-log-states)
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
+(defun my/org-checkbox-todo ()
+  "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+  (let ((todo-state (org-get-todo-state)) beg end)
+    (unless (not todo-state)
+      (save-excursion
+	(org-back-to-heading t)
+	(setq beg (point))
+	(end-of-line)
+	(setq end (point))
+	(goto-char beg)
+	(if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+			       end t)
+	    (if (match-end 1)
+		(if (equal (match-string 1) "100%")
+		    (unless (string-equal todo-state "DONE")
+		      (org-todo 'done))
+		  (unless (string-equal todo-state "TODO")
+		    (org-todo 'todo)))
+	      (if (and (> (match-end 2) (match-beginning 2))
+		       (equal (match-string 2) (match-string 3)))
+		  (unless (string-equal todo-state "DONE")
+		    (org-todo 'done))
+		(unless (string-equal todo-state "TODO")
+		  (org-todo 'todo)))))))))
+(add-hook 'org-checkbox-statistics-hook 'my/org-checkbox-todo)
+
+(setq org-confirm-babel-evaluate nil)
+
 ;; Org-ipython
 (use-package conda
   :ensure t
   :config
   (conda-env-initialize-interactive-shells)
   (conda-env-initialize-eshell)
-  (conda-env-autoactivate-mode))
+  (setq conda-anaconda-home "/home/tom/soft/anaconda3"))
 
-;; company-jedi
-(use-package company-jedi
+(use-package anaconda-mode
+  :ensure t)
+
+(use-package lsp-mode :ensure t)
+
+(use-package company-lsp
   :ensure t
+  :after lsp-mode
   :config
+  (push 'company-lsp company-backends))
+
+;; Other config
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "IN-PROGRESS(p)" "BLOCKED(b)" "|" "DONE(d)" "DELETED(x)")))
+(setq org-todo-keyword-faces
+      '(("TODO(t)" . org-warning)
+	("IN-PROGRESS(p)" . "yellow")
+	("BLOCKED(b)" . "orange")
+	("DONE(d)" . "green")
+	("DELETED(x)" . "gray")))
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"))
+
+(use-package org-roam
+  :ensure t
   :hook
-  ((python-mode . jedi:setup))
-  :init
-  (setq jedi:complete-on-dot t)
-  (setq jedi:use-shortcuts t)
-  (add-hook 'python-mode-hook
-	    (lambda () (add-to-list 'company-backends 'company-jedi))))
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "/home/tom/org-roam")
+  :bind (:map org-roam-mode-map
+	      (("C-c n l" . org-roam)
+	       ("C-c n f" . org-roam-find-file)
+	       ("C-c n g" . org-roam-graph-show))
+	      :map org-mode-map
+	      (("C-c n i" . org-roam-insert))
+	      (("C-c n I" . org-roam-insert-immediate))))
+;; Miscellaneous org mode stuff
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((sql . t)
+   (org . t)))
+(put 'upcase-region 'disabled nil)
+
+;; Org mode extra
+(setq org-agenda-files '("~/docs/todo"))
+(setq org-agenda-custom-commands
+      '(("c" "Simple agenda view"
+	 ((agenda "")
+	  (alltodo "")))))
+(bind-key "C-c a" 'org-agenda)
+
+(use-package chronos
+  :ensure t)
+(use-package helm-chronos
+  :ensure t
+  :after chronos
+  :bind (("C-c t" . helm-chronos-add-timer)))
