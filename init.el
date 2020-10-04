9a5374b6fea3eba811b7a2edf3e866914ad14e34
Tom Brantseg <tom.brantseg@gmail.com>
2020-10-04 11:40:01 -0500

    fixup! modularize

--- init.el
+++ init.el
@@ -1,13 +1,14 @@
 (add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
 (byte-recompile-directory (expand-file-name "modules" user-emacs-directory) 0)
 
-(defvar init-modules '("init-startup.el"
-	     "init-bootstrap.el"
-	     "init-appearance.el"
-	     "init-helm.el"
-	     "init-hydra.el"
-	     "init-ide.el"
-	     "init-python.el"
-	     "init-org.el"))	       
+(defvar init-modules
+  '("init-startup.el"
+    "init-bootstrap.el"
+    "init-appearance.el"
+    "init-helm.el"
+    "init-hydra.el"
+    "init-ide.el"
+    "init-python.el"
+    "init-org.el"))	       
 
 (mapc 'load init-modules)
